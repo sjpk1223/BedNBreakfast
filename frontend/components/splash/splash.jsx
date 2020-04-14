efa8fec2d19155495266850eeb88f369ff7e2fe8
Josh <prkjsh1223@gmail.com>
2020-04-14 09:28:10 -0400

    Added incomplete search table

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,1 +1,46 @@
+import React from "react";
+// simply view element
+class Splash extends React.Component {
+  constructor(props) {
+    super(props);
+    }
 
+  render() {
+    return (
+      <div className="splash-container">
+        <div className="splash-form">
+          <div className="splash-form-title">
+            Search for places or experiences
+          </div>
+          <div className="splash-search">
+            <label className="splash-search-label">
+              <div className="splash-search-title">Location</div>
+              <input
+                type="text"
+                className="splash-search-input"
+                placeholder="Add city, landmark, or address"
+              />
+            </label>
+            <div className="search-table-organizer">
+              <div className="splash-search-title">Check-in / Check-out</div>
+              <button className="splash-dates-button" type="button">
+                <div className="splash-dates-placeholder">Add dates</div>
+              </button>
+            </div>
+            <div className="search-table-organizer">
+              <div className="splash-guest-title">Guests</div>
+              <button className="splash-guest-button" type="button">
+                <div className="splash-guest-placeholder">Add guests</div>
+              </button>
+            </div>
+            <button className="splash-search-button" type="button">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Splash;
--- frontend/components/splash/splash_container.js
+++ frontend/components/splash/splash_container.js
@@ -1,1 +1,16 @@
-import NavbarContainer from '../splash/splash_container';
+// built-in function to subscribe react components to the store(redux)
+// containers job is to connect our components(frontend) to the store
+import { connect } from "react-redux";
+// import action we need for this container
+
+// import our sign up form component
+import Splash from "./splash";
+// msp(slices of our state) & mdp(dispatches functions) contains information sent to component through props
+// const mdp = (dispatch) => ({
+//   // createNewUser function that we imported and return
+//   // input formUser(which we determined in actions)(we get from the form)
+//   createNewUser: (formUser) => dispatch(createNewUser(formUser)),
+// });
+// passing it in sign up component.
+//does not rely on state so we do not need msp
+export default connect(null, null)(Splash);
