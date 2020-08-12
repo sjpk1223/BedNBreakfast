e05ed79a08d3ad13117154b856b4e2665a41b2d2
joshua <sjpk1223@gmail.com>
2020-08-12 15:13:32 -0400

    Set up map

---
+++ frontend/components/map/map.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import MarkerManager from '../../util/marker_manager';
+
+const getCoordsObj = latLng => ({
+    lat: latLng.lat(),
+    lng: latLng.lng()
+})
+
+class Map extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount() {
+        const mapOptions = {
+            center: { lat: 37, lng: -122},
+            zoom: 13
+        };
+
+        this.map = new google.maps.Map(this.mapNode, mapOptions);
+        this.MarkerManager = new MarkerManager(this.map)
+
+        if (this.props.singlePlace){
+            this.MarkerManager.updateMarkers([this.props.place]);
+        } else {
+            this.MarkerManager.updateMarkers([this.props.places]);
+            this.filterBounds();
+        }
+    }
+
+    filterBounds() {
+        this.map.addListener('idle', () => {
+            let latLongBnds = this.map.getBounds();
+            let northEastLat = latLongBnds.getNorthEast().lat();
+            let northEastLng = latLongBnds.getNorthEast().lng();
+            let southWestLat = latLongBnds.getSouthWest().lat();
+            let southWestLng = latLongBnds.getSouthWest().lng();
+            let bounds = { southWest: { lat: southWestLat, lng: southWestLng }, northEast: { lat: northEastLat, lng: northEastLng } };
+            this.props.updateFilter('bounds', bounds);
+            this.registerListeners();
+        })
+    }
+
+    handleClick(coords) {
+        const { history } = this.props;
+        history.push({
+            pathname: "places/new",
+            search: `lat=${coords.lat}&lng=${coords.lng}`
+        });
+    }
+
+    registerListeners() {
+        google.maps.event.addListener(this.map, 'idle', () => {
+            const { north, south, east, west } = this.map.getBounds().toJSON();
+            const bounds = {
+                northEast: { lat:north, lng:east},
+                southWest: { lat:south, lng:west}
+            }
+            this.props.updateFilter('bounds', bounds)
+        });
+
+        google.maps.event.addListener(this.map, 'click', (event) => {
+            const coords = getCoordsObj(event.latLng);
+            this.handleClick(coords);
+        })
+    }
+
+    componentDidUpdate(){
+        if (this.props.singlePlace) {
+            this.MarkerManager.updateMarkers([this.props.place])
+        } else {
+            this.MarkerManager.updateMarkers([this.props.places])
+        }
+    }
+
+    render() {
+        return (
+            <div id='map-container' ref={map => this.mapNode = map}></div>
+        )
+    }
+
+}
+
+export default withRouter(Map);
