26eb96304f8dd28bf589b29907f4b1d027baaf97
joshua <sjpk1223@gmail.com>
2020-08-13 23:10:00 -0400

    Added filter form

---
+++ frontend/components/filterform/filterform.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const handleChange = (filter, updateFilter) => e => (
+    updateFilter(filter, parseInt(e.currentTarget.value))
+);
+
+const FilterForm = ({ min_seating, max_seating, updateFilter }) => (
+    <div>
+        <span className="filter">Filter results:</span>
+        <br />
+        <label>Minimum Seats</label>
+        <input
+            type="number"
+            value={min_seating}
+            onChange={handleChange('min_seating', updateFilter)}
+        />
+        <br />
+        <label>Maximum Seats</label>
+        <input
+            type="number"
+            value={max_seating}
+            onChange={handleChange('max_seating', updateFilter)}
+        />
+    </div>
+);
+
+export default FilterForm;
