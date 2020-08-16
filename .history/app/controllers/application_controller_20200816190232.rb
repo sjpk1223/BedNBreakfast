f984131370565a21a823614b9237b582c53caf73
joshua <sjpk1223@gmail.com>
2020-08-16 19:05:07 -0400

    Debug 422

--- .history/app/controllers/application_controller_20200816190232.rb
+++ .history/app/controllers/application_controller_20200816190232.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 helper_method :logged_in?, :current_user, :ensure_logged_in
-
+protect_from_forgery with: :null_session
     def current_user
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
